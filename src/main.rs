d1a7d69095a7751f0d1bf4906cb7d1e5f35fc7b8
Gustavo Prezoto <gustavoprezotoboca@gmail.com>
2025-11-17 11:20:29 -0300

    Implemented support for println! macro.

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,14 @@ use core::panic::PanicInfo;
 mod vga_buffer;
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-
-    vga_buffer::WRITER.lock().write_str("Test\n").unwrap();
-    vga_buffer::WRITER.lock().write_str("Test\n").unwrap();
-    vga_buffer::WRITER.lock().write_str("Test\n").unwrap();
-    vga_buffer::WRITER.lock().write_str("Test\n").unwrap();
+    println!("Test");
+    println!("Test");
 
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -3,6 +3,23 @@ use lazy_static::lazy_static;
 use spin::Mutex;
 use volatile::Volatile;
 
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
+
 #[allow(dead_code)] // Tells the compiler to ignore not used coded warnings
 #[derive(Debug, Clone, Copy, PartialEq, Eq)] // Auto generate implementations for these traits
 #[repr(u8)] // Tells the compiler to represent each color with 1 byte digits (necessary for VGA)
